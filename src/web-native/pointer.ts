d240f15039babe8f0ee31d9c3fac429b8aa1c25a
henohenon <takeru.hawk.son@gmail.com>
2025-05-22 16:54:36 +0900

    feat: add pointer from mouse

--- src/web-native/pointer.ts
+++ src/web-native/pointer.ts
@@ -11,10 +11,10 @@ export type WrapEvent = 'pointerenter' | 'pointerleave' | 'pointerover' | 'point
 export type CancelEvent = 'pointercancel';
 type PointerNativeEvent = ButtonEvent | MoveEvent | WrapEvent | CancelEvent;
 
-export interface PointerInputOptions {
-    target?: EventTarget;
-    buttons?: Multiable<number>;
+export interface PointerInputOptions extends MouseInputOptions {
     events?: Multiable<PointerNativeEvent>;
+    pointerType?: Multiable<string>;
+    pointerId?: Multiable<number>;
 }
 
 const inputTypeAction: Record<PointerNativeEvent, DefaultAction> = {
@@ -28,30 +28,64 @@ const inputTypeAction: Record<PointerNativeEvent, DefaultAction> = {
     'pointercancel': 'end',
 };
 
-export const pointerInput = (
-    s: MultiSubject<WithPositionInputEvent>,
-    o?: PointerInputOptions
-) => {
-    const { target, events, buttons } = o || {};
+export function pointerInputBase<T extends PRXInputEvent>(
+    s: MultiSubject<T>,
+    mapEvent: (e: PointerEvent, action: DefaultAction) => T,
+    o?: PointerInputOptions,
+){
+    const { target, events, button, buttons, pointerType, pointerId } = o || {};
     const _target = target || document;
     const _events = events ? 
         multiableToArray(events) : 
-        ['pointerdown', 'pointerup', 'pointermove', 'pointerenter', 'pointerleave', 'pointerover', 'pointerout', 'pointercancel'] as PointerNativeEvent[];
+            ['pointerdown', 'pointerup', 'pointermove', 'pointerenter', 'pointerleave', 'pointerover', 'pointerout', 'pointercancel'] as PointerNativeEvent[];
+    const _button = button ? new Set(multiableToArray(button)) : undefined;
     const _buttons = buttons ? new Set(multiableToArray(buttons)) : undefined;
-    const isExec = (e: PointerEvent) => isEventBySetUndef(_buttons, e.button);
+    const _pointerType = pointerType ? new Set(multiableToArray(pointerType)) : undefined;
+    const _pointerId = pointerId ? new Set(multiableToArray(pointerId)) : undefined;
+    const isExec = (e: PointerEvent) => 
+        isEventBySetUndef(button, e.button) && 
+        isEventBySetUndef(_buttons, e.buttons) && 
+        isEventBySetUndef(_pointerType, e.pointerType) && 
+        isEventBySetUndef(_pointerId, e.pointerId);
 
-    return nativeInputBase<WithPositionInputEvent, PointerNativeEvent, PointerEvent>(
+    return nativeInputBase<T, PointerNativeEvent, MouseEvent>(
         s,
         _target,
         _events,
         inputTypeAction,
         isExec,
+        mapEvent
+    );
+}
+
+export const pointerInput = (
+    s: MultiSubject<PRXInputEvent>,
+    o?: PointerInputOptions
+) => {
+    return pointerInputBase<PRXInputEvent>(
+        s,
         (e: PointerEvent, action: DefaultAction) => ({
-            key: e.pointerId.toString(),
+            key: e.pointerType,
+            action,
+            time: e.timeStamp,
+        } as PRXInputEvent),
+        o
+    );
+}
+
+export const pointerInputWithPosition = (
+    s: MultiSubject<WithPositionInputEvent>,
+    o?: PointerInputOptions
+) => {
+    return pointerInputBase<WithPositionInputEvent>(
+        s,
+        (e: PointerEvent, action: DefaultAction) => ({
+            key: e.pointerType,
             action,
             time: e.timeStamp,
             x: e.clientX,
             y: e.clientY,
         } as WithPositionInputEvent),
+        o
     );
 }
