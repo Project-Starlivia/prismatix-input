e4f96dfff37d67f0d4a5f0f7e7ab48777b59fc60
henohenon <66393463+henohenon@users.noreply.github.com>
2025-05-22 10:00:00 +0900

    feat: add index/pointer to web-native

---
+++ src/web-native/index.ts
@@ -0,0 +1,57 @@
+import { Extension } from "typescript";
+import type { DefaultAction, PRXInputEvent } from "../events";
+import type { MultiSubject } from "../subject";
+import { multiableToArray } from "../utils";
+import type { Multiable } from "../utils";
+
+
+export interface WithPositionInputEvent extends PRXInputEvent {
+    x: number;
+    y: number;
+}
+
+export const isEventBySetUndef = <T>(set: Set<T> | undefined, value: T): boolean => {
+    if (!set) return true;
+    return set.has(value);
+} 
+
+export interface NativeInputOption<T extends PRXInputEvent, A extends string = DefaultAction> {
+    target?: EventTarget;
+    events?: Multiable<string>;
+    mapEvent?: (e: PointerEvent, action: A) => T;
+}
+
+export function nativeInputBase<T extends PRXInputEvent, ET extends string, EN extends Event, A extends string = DefaultAction>(
+    s: MultiSubject<T>,
+    target: EventTarget,
+    events: ET[],
+    actionMap: Record<ET, A>,
+    isEvent: (e: EN) => boolean,
+    mapEvent: (e: EN, action: A) => T
+) {
+    const _subjects = multiableToArray(s);
+    
+    const listeners = events.map(type => {
+        const handler = (e: EN) => {
+            const action = actionMap[type];
+            if (!action) throw new Error(`No action for event type: ${type}`);
+            if (!isEvent(e)) return;
+            const event = mapEvent(e, action);
+            for (const stream of _subjects) {
+                stream.next(event);
+            }
+        }
+        const fn = (e: Event) => handler(e as EN);
+        
+        target.addEventListener(type, fn);
+        return () => target.removeEventListener(type, fn);
+    });
+
+    const dispose = () => {
+        for (const remove of listeners) {
+            remove();
+        }
+    }
+
+    return { dispose };
+}
---
+++ src/web-native/pointer.ts
@@ -0,0 +1,57 @@
+import type { DefaultAction } from "../events";
+import type { MultiSubject } from "../subject";
+import { multiableToArray } from "../utils";
+import type { Multiable } from "../utils";
+import { isEventBySetUndef, nativeInputBase } from ".";
+import type { WithPositionInputEvent } from "./index";
+
+export type ButtonEvent = 'pointerdown' | 'pointerup';
+export type MoveEvent = 'pointermove';
+export type WrapEvent = 'pointerenter' | 'pointerleave' | 'pointerover' | 'pointerout';
+export type CancelEvent = 'pointercancel';
+type PointerNativeEvent = ButtonEvent | MoveEvent | WrapEvent | CancelEvent;
+
+export interface PointerInputOptions {
+    target?: EventTarget;
+    buttons?: Multiable<number>;
+    events?: Multiable<PointerNativeEvent>;
+}
+
+const inputTypeAction: Record<PointerNativeEvent, DefaultAction> = {
+    'pointerdown': 'start',
+    'pointerup': 'end',
+    'pointermove': 'move',
+    'pointerenter': 'start',
+    'pointerleave': 'end',
+    'pointerover': 'start',
+    'pointerout': 'end',
+    'pointercancel': 'end',
+};
+
+export const pointerNativeInput = (
+    s: MultiSubject<WithPositionInputEvent>,
+    o?: PointerInputOptions
+) => {
+    const { target, events, buttons } = o || {};
+    const _target = target || document;
+    const _events = events ? 
+        multiableToArray(events) : 
+        ['pointerdown', 'pointerup', 'pointermove', 'pointerenter', 'pointerleave', 'pointerover', 'pointerout', 'pointercancel'] as PointerNativeEvent[];
+    const _buttons = buttons ? new Set(multiableToArray(buttons)) : undefined;
+    const isExec = (e: PointerEvent) => isEventBySetUndef(_buttons, e.button);
+
+    return nativeInputBase<WithPositionInputEvent, PointerNativeEvent, PointerEvent>(
+        s,
+        _target,
+        _events,
+        inputTypeAction,
+        isExec,
+        (e: PointerEvent, action: DefaultAction) => ({
+            key: e.pointerId.toString(),
+            action,
+            time: e.timeStamp,
+            x: e.clientX,
+            y: e.clientY,
+        } as WithPositionInputEvent),
+    );
+}
