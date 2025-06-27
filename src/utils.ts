c3206c066be22a348f1b2d7d9f4b0fa3dce3e9bb
henohenon <66393463+henohenon@users.noreply.github.com>
2025-06-27 10:23:50 +0900

    chore: update `tsconfig.build.json`, dependencies, and remove unused utilities
    Updated `tsconfig.build.json` to include new path mappings and adjust build options. Modified build script to include `tsc-alias` and updated dependencies in `package.json`. Removed unused utility functions from `utils.ts` for a cleaner codebase.

--- src/utils.ts
+++ src/utils.ts
@@ -7,11 +7,3 @@ export function multiableToArray<T>(multi: Multiable<T>): readonly T[] {
 export type EmptyObject = Record<string, never>;
 
 export const EMPTY_OBJECT: EmptyObject = Object.freeze({});
-
-export function createId(prefix: string, counter: number): string {
-    return counter === 0 ? prefix : `${prefix}${counter}`;
-}
-
-export function isNonNullable<T>(value: T | null | undefined): value is T {
-    return value != null;
-}
